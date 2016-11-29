acf000fa1c73c02a564c4909349fb27fa2d9e9e4
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-11-29 13:57:32 -0800

    wip renderer.

--- renderer.php
+++ renderer.php
@@ -41,35 +41,178 @@ class qtype_formassmnt_renderer extends qtype_renderer {
 
         $question = $qa->get_question();
 
-        $questiontext = $question->format_questiontext($qa);
-        $placeholder = false;
-        if (preg_match('/_____+/', $questiontext, $matches)) {
-            $placeholder = $matches[0];
+        $responseoutput = $this->page->get_renderer('qtype_formassmnt', 'format_editor');
+
+        // Answer field.
+        $step = $qa->get_last_step_with_qt_var('answer');
+
+
+        if (!$step->has_qt_var('answer') && empty($options->readonly)) {
+            // Question has never been answered, fill it with response template.
+            $step = new question_attempt_step(array('answer'=>$question->responsetemplate));
         }
-        $input = '**subq controls go in here**';
 
-        if ($placeholder) {
-            $questiontext = substr_replace($questiontext, $input,
-                    strpos($questiontext, $placeholder), strlen($placeholder));
+        if (empty($options->readonly)) {
+            $answer = $responseoutput->response_area_input('answer', $qa,
+                $step, $question->responsefieldlines, $options->context);
+
+        } else {
+            $answer = $responseoutput->response_area_read_only('answer', $qa,
+                $step, $question->responsefieldlines, $options->context);
         }
 
-        $result = html_writer::tag('div', $questiontext, array('class' => 'qtext'));
+        $result = '';
+        $result .= html_writer::tag('div', $question->format_questiontext($qa),
+            array('class' => 'qtext'));
 
-        /* if ($qa->get_state() == question_state::$invalid) {
+        $result .= html_writer::start_tag('div', array('class' => 'ablock'));
+        $result .= html_writer::tag('div', $answer, array('class' => 'answer'));
+        $result .= html_writer::end_tag('div');
+
+        if ($qa->get_state() == question_state::$invalid) {
             $result .= html_writer::nonempty_tag('div',
-                    $question->get_validation_error(array('answer' => $currentanswer)),
-                    array('class' => 'validationerror'));
-        }*/
+                $question->get_validation_error(array('answer' => $answer)),
+                array('class' => 'validationerror'));
+        }
+
         return $result;
     }
 
     public function specific_feedback(question_attempt $qa) {
-        // TODO.
-        return '';
+        $question = $qa->get_question();
+
+        $answer = $question->get_matching_answer(array('answer' => $qa->get_last_qt_var('answer')));
+        if (!$answer || !$answer->feedback) {
+            return '';
+        }
+
+        return $question->format_text($answer->feedback, $answer->feedbackformat,
+            $qa, 'question', 'answerfeedback', $answer->id);
+    }
+}
+
+
+/**
+ * A format renderer for formative assessments where the student should use the HTML
+ * editor without the file picker.
+ *
+ * Copied and modified from the Essay question type for our purposes.
+ *
+ * @copyright  2016 The Regents of the University of California
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_formassmnt_format_editor_renderer extends plugin_renderer_base {
+    protected function class_name() {
+        return 'qtype_formassmnt_editor';
     }
 
-    public function correct_response(question_attempt $qa) {
-        // TODO.
+    public function response_area_read_only($name, $qa, $step, $lines, $context) {
+        return html_writer::tag('div', $this->prepare_response($name, $qa, $step, $context),
+            array('class' => $this->class_name() . ' qtype_formassmnt_editor readonly'));
+    }
+
+    public function response_area_input($name, $qa, $step, $lines, $context) {
+        global $CFG;
+        require_once($CFG->dirroot . '/repository/lib.php');
+
+        $inputname = $qa->get_qt_field_name($name);
+        $responseformat = $step->get_qt_var($name . 'format');
+        $id = $inputname . '_id';
+
+        $editor = editors_get_preferred_editor($responseformat);
+        $strformats = format_text_menu();
+        $formats = $editor->get_supported_formats();
+        foreach ($formats as $fid) {
+            $formats[$fid] = $strformats[$fid];
+        }
+
+        list($draftitemid, $response) = $this->prepare_response_for_editing(
+            $name, $step, $context);
+
+        $editor->use_editor($id, $this->get_editor_options($context),
+            $this->get_filepicker_options($context, $draftitemid));
+
+        $output = '';
+        $output .= html_writer::start_tag('div', array('class' =>
+            $this->class_name() . ' qtype_formassmnt_response'));
+
+        $output .= html_writer::tag('div', html_writer::tag('textarea', s($response),
+            array('id' => $id, 'name' => $inputname, 'rows' => $lines, 'cols' => 60)));
+
+        $output .= html_writer::start_tag('div');
+        if (count($formats) == 1) {
+            reset($formats);
+            $output .= html_writer::empty_tag('input', array('type' => 'hidden',
+                'name' => $inputname . 'format', 'value' => key($formats)));
+
+        } else {
+            $output .= html_writer::label(get_string('format'), 'menu' . $inputname . 'format', false);
+            $output .= ' ';
+            $output .= html_writer::select($formats, $inputname . 'format', $responseformat, '');
+        }
+        $output .= html_writer::end_tag('div');
+
+        $output .= $this->filepicker_html($inputname, $draftitemid);
+
+        $output .= html_writer::end_tag('div');
+        return $output;
+    }
+
+    /**
+     * Prepare the response for read-only display.
+     * @param string $name the variable name this input edits.
+     * @param question_attempt $qa the question attempt being display.
+     * @param question_attempt_step $step the current step.
+     * @param object $context the context the attempt belongs to.
+     * @return string the response prepared for display.
+     */
+    protected function prepare_response($name, question_attempt $qa,
+        question_attempt_step $step, $context) {
+        if (!$step->has_qt_var($name)) {
+            return '';
+        }
+
+        $formatoptions = new stdClass();
+        $formatoptions->para = false;
+        return format_text($step->get_qt_var($name), $step->get_qt_var($name . 'format'),
+            $formatoptions);
+    }
+
+    /**
+     * Prepare the response for editing.
+     * @param string $name the variable name this input edits.
+     * @param question_attempt_step $step the current step.
+     * @param object $context the context the attempt belongs to.
+     * @return string the response prepared for display.
+     */
+    protected function prepare_response_for_editing($name,
+        question_attempt_step $step, $context) {
+        return array(0, $step->get_qt_var($name));
+    }
+
+    /**
+     * @param object $context the context the attempt belongs to.
+     * @return array options for the editor.
+     */
+    protected function get_editor_options($context) {
+        return array('context' => $context);
+    }
+
+    /**
+     * @param object $context the context the attempt belongs to.
+     * @param int $draftitemid draft item id.
+     * @return array filepicker options for the editor.
+     */
+    protected function get_filepicker_options($context, $draftitemid) {
+        return array('return_types'  => FILE_INTERNAL | FILE_EXTERNAL);
+    }
+
+    /**
+     * @param string $inputname input field name.
+     * @param int $draftitemid draft file area itemid.
+     * @return string HTML for the filepicker, if used.
+     */
+    protected function filepicker_html($inputname, $draftitemid) {
         return '';
     }
 }
