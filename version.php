cfbff7507a8dcdc7417ed2c55024facf5db8259d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-09-27 09:27:16 -0700

    version bump to M4.4 and corresponding CI workflow changes.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_knowledgecheck';
-$plugin->version   = 2024061000;
-$plugin->release = 'v4.3';
-$plugin->requires = 2023100400;
-$plugin->supported = [403, 403];
+$plugin->version = 2024092700;
+$plugin->release = 'v4.4';
+$plugin->requires = 2024041600;
+$plugin->supported = [404, 404];
 $plugin->maturity  = MATURITY_STABLE;
