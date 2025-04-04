11dab46f62d4c20c2ad8e9d7297f7d6e8752ba53
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-04-04 14:56:36 -0700

    version bump.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_knowledgecheck';
-$plugin->version = 2025022000;
+$plugin->version = 2025022001;
 $plugin->release = 'v4.5';
 $plugin->requires = 2024100100;
 $plugin->supported = [405, 405];
