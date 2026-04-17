2607b9046425b609c6c2c34a4df974d0cc8004dc
Miriam Leung <miriam.leung@ucsf.edu>
2026-04-17 10:35:43 -0700

    remove MOODLE_INTERNAL check in new renderer_format_editor.php file.

--- renderer_format_editor.php
+++ renderer_format_editor.php
@@ -22,8 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * A format renderer for knowledge checks where the student should use the HTML editor without the file picker.
  *
